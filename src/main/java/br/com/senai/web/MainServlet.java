f4a1bfbdabc1228b6a6a498d79548e6002fd2a80
senaidf <contato@sistemafibra.com.br>
2019-06-24 21:26:52 -0300

    [add] - criação dos verbos de requisição HTTP

--- src/main/java/br/com/senai/web/MainServlet.java
+++ src/main/java/br/com/senai/web/MainServlet.java
@@ -8,11 +8,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
+@WebServlet("/")
 public class MainServlet extends HttpServlet {
     @Override
-    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("Método GET dos verbos HTTP");
+    }
 
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.getWriter().println("Método POST dos verbos HTTP");
+    }
+
+    @Override
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.getWriter().println("Método PUT dos verbos HTTP");
+    }
+
+    @Override
+    public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.getWriter().println("Método DELETE dos verbos HTTP");
     }
 }
